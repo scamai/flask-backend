1b2fd006044c192d2a9c46d8e628791532c1a39e
Rachel Cheng <rachel.cheng@bluemarvel.ai>
2024-08-14 00:12:45 -0400

    first working version

--- app.py
+++ app.py
@@ -10,22 +10,25 @@ import cv2
 
 app = Flask(__name__)
 
+device = torch.device('cpu')
 model=Detector()
-model=model.cuda()
-cnn_sd=torch.load('model.pth', map_location=torch.device('cpu'))
+# model=model.cuda()
+# model.load_state_dict(torch.load('model.pth', map_location=device))
+cnn_sd=torch.load('model.pth', map_location=device)
 model.load_state_dict(cnn_sd)
+model = model.to(device)
 model.eval()
 
-def preprocess_image(image):
+def preprocess_image(uploadedImage):
     # add code for preprocessing
-    image = cv2.imread(example_fake_image)
+    image = cv2.imread(uploadedImage)
 
     # Convert BGR image to RGB image
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     image = image.astype('double')
 
-    face_detector = get_model("resnet50_2020-07-20", max_size=max(image.shape),device='cuda:0')
+    face_detector = get_model("resnet50_2020-07-20", max_size=max(image.shape),device=device)
     face_detector.eval()
     face_list=extract_face(image,face_detector, (128, 128))
     return face_list
@@ -38,15 +41,17 @@ def predict():
         return jsonify({'error': 'No file provided'}), 400
     
     file = request.files['file']
-    img_bytes = file.read()
+    image_path = f'/tmp/{file.filename}'
+    file.save(image_path)
+    # img_bytes = file.read()
 
-    face_list = preprocess_image(img_bytes)
+    face_list = preprocess_image(image_path)
 
     with torch.no_grad():
-       img=torch.tensor(face_list).to('cuda:0').float()/255
+       img=torch.tensor(face_list).to(device).float()/255
        pred=model(img).softmax(1)[:,1].cpu().data.numpy().tolist()
 
     return jsonify({'fakeness': round(max(pred), 4)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=True)
