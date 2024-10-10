65e1c1facbd7cefe6300b5a7bb36b15e689aeb23
Hengwei Xu <david.xuhengwei@gmail.com>
2024-10-10 19:21:39 -0400

    Update calibration coefficients

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def predict():
         img = torch.tensor(face_list).to(device).float() / 255
         pred = model(img).softmax(1)[:, 1].cpu().data.numpy().tolist()
         y_logits = logit(pred)
-        corrected = expit(4.915e-01 * y_logits + 3.939e-01)
+        corrected = expit(3.563e-01 * y_logits + 5.780e-02)
 
     return jsonify({'fakeness': round(float(corrected[0]), 4)})
 
