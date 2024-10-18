7c152390092b1ec63d03d75ec9ad6e12975a5fdf
Hengwei Xu <david.xuhengwei@gmail.com>
2024-10-17 23:33:01 -0500

    david: clean up app.py and avoid saving user image

--- app.py
+++ app.py
@@ -8,24 +8,22 @@ import numpy as np
 from scipy.special import logit, expit
 
 app = Flask(__name__)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 model = Detector()
-cnn_sd = torch.load('checkpoint_epoch_49_efficientnet-b4_bs_12_epoch_50_imgSize_380.pt', map_location=device)
+cnn_sd = torch.load('checkpoint_epoch_49_efficientnet-b4_bs_12_epoch_50_imgSize_380.pt', map_location=device, weights_only=True)
 new_sd = {}
 for k, v in cnn_sd.items():
     new_sd['net.' + k] = v
-model.load_state_dict(cnn_sd)
-model = model.to(device)
+model.load_state_dict(new_sd)
 model.eval()
 
 
-def extract_face(frame, model, image_size=(380, 380)):
-    faces = model.predict_jsons(frame)
+def extract_face(frame, face_detector_model, image_size=(380, 380)):
+    faces = face_detector_model.predict_jsons(frame)
 
     if len(faces) == 0:
         print('No face is detected')
         return []
-
     cropped_faces = []
     for face_idx in range(len(faces)):
         x0, y0, x1, y1 = faces[face_idx]['bbox']
@@ -46,19 +44,21 @@ def crop_face(img, bbox):
     w1_margin = w / 2
     h0_margin = h / 2
     h1_margin = h / 2
-    y0_new = max(0, y0 - h0_margin)
-    y1_new = min(height, y1 + h1_margin + 1)
-    x0_new = max(0, x0 - w0_margin)
-    x1_new = min(weight, x1 + w1_margin + 1)
-
+    y0_new = int(max(0, y0 - h0_margin))
+    y1_new = int(min(height, y1 + h1_margin + 1))
+    x0_new = int(max(0, x0 - w0_margin))
+    x1_new = int(min(weight, x1 + w1_margin + 1))
     return img[y0_new:y1_new, x0_new:x1_new]
 
 
-def preprocess_image(uploaded_image):
-    image = cv2.imread(uploaded_image)
-    image = image.squeeze().numpy()
-
-    face_detector = get_model("resnet50_2020-07-20", max_size=2048, device=device)
+def preprocess_image(file_storage):
+    image_bytes = file_storage.read()
+    # Convert raw bytes into a NumPy array
+    image_np = np.frombuffer(image_bytes, np.uint8)
+    # Decode the image as a NumPy array
+    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    face_detector = get_model(model_name="resnet50_2020-07-20", max_size=380, device=device)
     face_detector.eval()
     face_list = extract_face(image, face_detector)
     return face_list
@@ -70,22 +70,19 @@ def predict():
         return jsonify({'error': 'No file provided'}), 400
 
     file = request.files['file']
-    image_path = f'/tmp/{file.filename}'
-    file.save(image_path)
-
-    face_list = preprocess_image(image_path)
-
+    face_list = preprocess_image(file)
     if len(face_list) == 0:
         return jsonify({'error': 'No face detected'})
 
     with torch.no_grad():
         img = torch.tensor(face_list).to(device).float() / 255
-        pred = model(img).softmax(1)[:, 1].cpu().data.numpy().tolist()
+        pred = model(img).softmax(1)[:, 1].cpu().data.numpy().tolist()[0]
         y_logits = logit(pred)
         corrected = expit(3.563e-01 * y_logits + 5.780e-02)
 
-    return jsonify({'fakeness': round(float(corrected[0]), 4)})
+    return jsonify({'fakeness': round(float(corrected), 4)})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5090, debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5090)
