b55871f75915c8a6f126f0625f4be56503dfdcbb
Sunny Chan <sunnychan@Sunnys-MacBook-Pro.local>
2024-11-20 08:28:39 -0800

    add health check api end

--- app.py
+++ app.py
@@ -90,6 +90,13 @@ def predict():
     return jsonify({'fakeness': round(float(deep_fake_defender_pred[0]), 4)})
 
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({
+        'status': 'healthy'
+    }), 200
+
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=5090, threads=1)
